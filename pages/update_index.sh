b54e2ae83d8e01098285e39f6e16fe0f80341ced
Peter Wang <happypeter1983@gmail.com>
2011-02-14 21:54:59 +0800

    update_index.sh

---
+++ pages/update_index.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+OUTPUT=index.html
+if [ -f $OUTPUT ]
+then
+    echo  rm old index.html
+    rm $OUTPUT
+fi
+
+echo """
+
+<head>
+<link rel="stylesheet" type="text/css" href="../css/peter.css" />
+<title>PIKI</title>
+</head>
+<a  href="http://happypeter.github.com/piki/index.html">Go to Site Home</a>
+
+<br><center><h1>Index</h1></center>
+
+""">$OUTPUT
+for file in `ls|grep md|sort`
+do
+    echo -n '<a href="' >>$OUTPUT
+    echo -n $file|awk -F"." '{print $1}' >>$OUTPUT
+    echo -n '.html' >>$OUTPUT
+    echo '">' >>$OUTPUT
+    title=`cat $file|grep "title:"|awk -F":" '{print $2}'`
+    echo -n $title >>$OUTPUT
+    echo '</a>' >>$OUTPUT
+    echo '<br>'>>$OUTPUT
+
+done
